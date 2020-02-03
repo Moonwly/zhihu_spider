9edcde3a23d13e387bc3baa38199abf6eb24cb52
Moonwly <1241076850@qq.com>
2020-02-03 22:05:15 +0800

    modify

--- zhihu_spider/misc/all_secret_set.py
+++ zhihu_spider/misc/all_secret_set.py
@@ -2,8 +2,8 @@ import pymysql
 
 # 数据库相关配置
 mysql_config = {
-    'host': '106.15.204.111',
-    # 'host': '127.0.0.1',
+    # 'host': '106.15.204.111',
+    'host': '127.0.0.1',
     'port': 3306,
     'user': 'root',
     'password': '123456',
